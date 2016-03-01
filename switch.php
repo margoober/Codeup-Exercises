d72192b7478e43b4fb08c8528f990b724b3ebd66
Margot McMahon <andlmcm@Andrews-MacBook-Air.local>
2016-03-01 14:36:10 -0600

    Finished day of week switch

--- switch.php
+++ switch.php
@@ -4,30 +4,40 @@ date_default_timezone_set('America/Chicago');
 
 $dayOfWeek = date('D');
 switch($dayOfWeek) {
- 	case Mon:
+ 	case "Mon":
         echo "It's Monday!" . PHP_EOL;
  		break;
- 	case Tue:
+ 	case "Tue":
  		echo "It's Tuesday!" . PHP_EOL;
  		break;
- 	case Wed:
+ 	case "Wed":
 		echo "It's Wednesday!" . PHP_EOL;
  		break;
- 	case Thu:
+ 	case "Thu":
 		echo "It's Thursday!" . PHP_EOL;
  		break;
- 	case Fri:
+ 	case "Fri":
  		echo "It's Friday!" . PHP_EOL;
  		break;
- 	case Sat:
+ 	case "Sat":
  		echo "It's Saturday!" . PHP_EOL;
  		break;
- 	case Sun:
+ 	case "Sun":
  		echo "It's Sunday!" . PHP_EOL;
  		break;
  	default:
  		echo "That's not a day!" . PHP_EOL;
  		break;
 }
-$dayOfYear = date('z');
-echo "It's the {$dayOfYear}th day of the year!" . PHP_EOL;
+$dayOfMonth = date('j');
+$ordinal = date("S");
+$leap = date('L');
+echo "It's the {$dayOfMonth}{$ordinal} day of the month!" . PHP_EOL;
+switch($leap) {
+ 	case 1:
+        echo "It's a LEAP YEAR!" . PHP_EOL;
+ 		break;
+ 	default:
+ 		echo "It's not a leap year!" . PHP_EOL;
+ 		break;
+ }
