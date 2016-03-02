5288ac8441f7eb449d24c3ef875c81c1d8253d5a
Margot McMahon <amcmahon@nes.ru>
2016-03-02 13:12:48 -0600

    Arithmetic and INternal Functions Exercises - Complete

--- arithmetic.php
+++ arithmetic.php
@@ -1,30 +1,55 @@
 <?php
 
-function add($a, $b)
-{
-    return $a + $b;
+function add($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+		echo "{$a} plus {$b} is ";
+    	return $a + $b;
+	} else {
+		throwErrorMessage($a, $b);
+	}
 }
 
-function subtract($a, $b)
-{
-	return $a - $b;
+function subtract($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+		echo "{$a} minus {$b} is ";
+    	return $a - $b;
+	} else {
+		throwErrorMessage($a, $b);
+	}
 }
-
-function multiply($a, $b)
-{
-	return $a * $b;
+function multiply($a, $b) {
+	if (is_numeric($a) && is_numeric($b)) {
+		echo "{$a} times {$b} is ";
+    	return $a * $b;
+	} else {
+		throwErrorMessage($a, $b);
+	}
 }
-
-function divide($a, $b)
-{
-	return $a / $b;
+#note: switch the is_numeric check and the is_zero check in order to avoid PHP registering non-number data inputs as 0, and then echoing "Cant devide by zeorr" (switch === to ==)
+function divide($a, $b) {
+	if ($b === 0) {
+		echo "Cant devide by zeorr";
+	} else if (is_numeric($a) && is_numeric($b)) {
+		echo "{$a} divided by {$b} is ";
+		return $a / $b;
+	} else {
+		throwErrorMessage($a, $b);
+	}
 }
 
 function mod($a, $b) {
-	return($a % $b);
+	if (is_numeric($a) && is_numeric($b)) {
+		echo "{$a} mod {$b} is ";
+    	return $a % $b;
+	} else {
+		throwErrorMessage($a, $b);
+	}
 }
-echo add(2, 3) . PHP_EOL;
+function throwErrorMessage($a, $b) {
+	echo "ERROR: BOth arguments gotta b nubmers, and either {$a} or {$b} isnt\n";
+}
+echo add(1, 3) . PHP_EOL;
 echo subtract(2, 3) . PHP_EOL;
-echo multiply(2, 3) . PHP_EOL;
-echo divide(2, 3) . PHP_EOL;
+echo multiply(2, 5) . PHP_EOL;
+echo divide(2, "b") . PHP_EOL;
 echo mod(21, 3) . PHP_EOL;
---
+++ sandbox.php
@@ -0,0 +1,42 @@
+<?PHP
+//inspection function
+function inspect($parameter) {
+	$type = gettype($parameter);
+	if (is_null($parameter)) {
+		echo "The value is NOLE!" . PHP_EOL;
+	} else if (is_bool($parameter)) {
+		if ($parameter ==1) {
+			$parameter = "true";
+		} else {
+			$parameter = false;
+		}
+		echo "It's is a {$parameter} bool!" . PHP_EOL;
+	} else if (is_string($parameter) && $parameter == "") {
+		echo "It's a empy Strang!" . PHP_EOL;
+	} else {
+		if (is_array($parameter)) {
+			$parameter = serialize($parameter);
+		}
+		echo "Inspecting {$parameter}:" . PHP_EOL;
+		print_r("The type of \"$parameter\" is {$type}" . PHP_EOL);
+	}
+}
+//variables
+$string1 = "I'm am a smartre"; //0
+$string2 = ''; //1
+$array1 = []; //2
+$array2 = [1, 2, 3]; //3
+$bool1 = true; //4
+$bool2 = false; //5
+$num1 = 0; //6
+$num2 = 0.0; //7
+$num3 = 12; //8
+$num4 = 14.4; //9
+$null = NULL; //10
+//array of all
+$data = array($string1, $string2, $array1, $array2, $bool1, $bool2, $num1, $num2, $num3, $num4, $null);
+//foreach loop
+foreach ($data as $key => $datum) {
+	echo "Key {$key}: ";
+	inspect($datum);
+}
