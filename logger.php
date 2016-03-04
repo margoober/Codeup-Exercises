db27376931f8b766e210e3087846b25092b518d3
Margot McMahon <amcmahon@nes.ru>
2016-03-04 15:06:11 -0600

    Dynamic error/info message logger - fwrite()

---
+++ logger.php
@@ -0,0 +1,21 @@
+<?php
+
+function logMessage($logLevel, $message)
+{
+    // todo - complete this function
+    //log to file
+    $filename = "newlog-" . date("d-m-Y") . ".log";
+    $handle = fopen($filename, 'a');
+    $string = date("d-m-Y") . " " . date("H:i:s") . " " . $logLevel . " " . $message . PHP_EOL; 
+    fwrite($handle, $string);
+    fclose($handle);
+}
+
+function logInfo($message = "This is an info message!"){
+	logMessage("INFO", $message);
+}
+function logError($message = "This is an error message!"){
+	logMessage("ERROR", $message);
+}
+logInfo();
+logError();
