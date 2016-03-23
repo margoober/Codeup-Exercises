a0fee2eb54794e35dfebe35ead05ebe19f8a5ed6
Margot McMahon <amcmahon@nes.ru>
2016-03-23 14:14:35 -0500

    log file chipped away at???

--- log.php
+++ log.php
@@ -2,18 +2,38 @@
 
 class Log {
 
-	public $filename;
+	private $filename;
+	private $handle;
+	private $date;
 
-	function logMessage($logLevel, $message) {
+	//construct function\
+
+	public function __construct($prefix = 'log'){
+		$this->date = date('Y-m-d');
+		$this->filename = $prefix . '-' . $this->date . '.log';
+		if (is_writeable($this->filename)) {
+			$this->handle = fopen($this->filename, 'a');	
+		}
+	}
+
+	private function setFilename($filename){
+		if (! is_string($filename)) {
+			echo 'The property $filename needs to be a string. You gave ' . gettype($filename) . PHP_EOL;
+			die();
+		}
+	}
+
+	public function logMessage($logLevel, $message) {
+		$time = date('H:i:s');
 		$date = date('y-m-d');
 		$filename = $prefix . $date . ".log";
 		$handle = fopen($filename, a);
 	}
 
-	function info (){
+	function logInfo (){
 	}
 
-	function error(){
+	function logError(){
 		
 	}
 
