9bdebaa5aac82afcba7a2445a3eec333b3e5cd66
Margot McMahon <amcmahon@nes.ru>
2016-03-24 12:30:55 -0500

    class Model before spring break

--- model.php
+++ model.php
@@ -1,1 +1,68 @@
 <?php
+
+class Model
+{
+	private $attributes = [];
+	private $species;
+	protected static $table;
+
+	public function __set($key, $value){
+		$this->attributes[$key] = $value;
+	}
+
+	public function __get($key) {
+		if (array_key_exists($key, $this->attributes)) {
+
+			return $this->attributes[$key];
+
+		} else {
+
+			return null;
+
+		}
+	}
+
+	//add a static method named getTableName() that returns the value of the static property $table;
+	public static function getTableName() {
+		return static::$table;
+	}
+}
+
+//testing it out!
+/*
+$person = new Model;
+
+$person->species = "humbean";
+echo '$person->species is: ' . $person->species . PHP_EOL;
+
+$person->hatColor = "devils' blue";
+echo $person->hatColor . PHP_EOL;
+
+$person->biggestRegret = "being bornt";
+echo $person->biggestRegret . PHP_EOL;
+
+$person->favoriteFinger = "devil's finger";
+echo $person->favoriteFinger . PHP_EOL;
+
+echo "___________________" . PHP_EOL;
+
+var_dump($person);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ user.php
@@ -0,0 +1,11 @@
+<?php
+require_once "model.php";
+
+class User extends Model
+{
+	protected static $table = "users";
+}
+
+echo User::getTableName();
+
+throw new Exception("here comes some bullshit");
